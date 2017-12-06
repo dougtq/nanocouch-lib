de0cd25bb538a4d5c88f689bcadf8ebd615b5d0a
Douglas Alves <douglas.expalves@gmail.com>
2017-12-06 15:05:31 -0200

    db class config

---
+++ src/db/index.js
@@ -0,0 +1,30 @@
+import Nano from 'nano'
+import envs from '../config/env'
+
+export default class couchDB {
+  constructor (db) {
+    this.conn = envs.vars.db_host
+    this.db = envs.vars.db_name
+  }
+
+  connect () {
+    let conn = Nano(this.conn)
+    return conn.db.use(this.db)
+  }
+
+  getConn () {
+    return this.conn
+  }
+
+  setConn (conn) {
+    this.conn = conn
+  }
+
+  getDB () {
+    return this.db
+  }
+
+  setDB (db) {
+    this.db = db
+  }
+}
